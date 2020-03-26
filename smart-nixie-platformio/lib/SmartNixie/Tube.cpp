ee81a170e2cc11e7392f19a8879e3d37cd496beb
unknown <danielbene@protonmail.com>
2020-03-26 18:42:11 +0100

    Add tested base version of the Tube controller class

--- smart-nixie-platformio/lib/SmartNixie/Tube.cpp
+++ smart-nixie-platformio/lib/SmartNixie/Tube.cpp
@@ -4,22 +4,21 @@ Tube::Tube(int pins[4], Adafruit_MCP23017 mcpObj) {
     mcp = mcpObj;
     for (int i = 0; i < 4; i++) {
         ioPins[i] = pins[i];
-        pinMode(ioPins[i], OUTPUT);
+        mcp.pinMode(ioPins[i], OUTPUT);
     }
 }
 
-void writeNum(int num) {
-    int binaryNum[4]; 
+void Tube::writeNum(int num) {
+    int binaryNum[4] = {0, 0, 0, 0};
 
-    int i = 0; 
+    int i = 3;
     while (num > 0) { 
         binaryNum[i] = num % 2; 
         num = num / 2; 
-        i++; 
+        i--;
     }
 
     for (int i = 0; i < 4; i++) {
-        //digitalWrite(ioPins[i], binaryNum[i]);
         mcp.digitalWrite(ioPins[i], binaryNum[i]);
     }
 }
--- smart-nixie-platformio/src/main.cpp
+++ smart-nixie-platformio/src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include "Adafruit_MCP23017.h"
+#include "Tube.h"
+
+Adafruit_MCP23017 mcpObj;
+int pins[] = {11, 10, 9, 8};
+int value = 0;
+
+// 1100 value turns off the tube!
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  // these go in the clock class later
+  mcpObj.begin(); // pinmode setup in Tube obj constructor, so begin needs before that
+  Tube firstTube(pins, mcpObj);
+
+  firstTube.writeNum(3);
+  delay(1000);
+  firstTube.writeNum(0);
+  delay(1000);
+  firstTube.writeNum(9);
+  delay(1000);
+  firstTube.writeNum(5);
+  delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }

b190b3b5ee16b2064f9db11cfcab40d429d34e4f
Bene Dániel <bene.daniel@ibcontroll.hu>
2020-04-10 13:50:00 +0200

    Add base version of the sensor controller class

--- smart-nixie-platformio/lib/SmartNixie/SN_Sensor.cpp
+++ smart-nixie-platformio/lib/SmartNixie/SN_Sensor.cpp
@@ -1,5 +1,39 @@
 #include "SN_Sensor.h"
 
 SN_Sensor::SN_Sensor() {
-    // NOP - used by the object declarations in Display.h
+    // NOP
+}
+
+SN_Sensor::SN_Sensor(SN_Display snDisp) {
+    disp = snDisp;
+    tempUnit = BME280::TempUnit(BME280::TempUnit_Celsius);
+    presUnit = BME280::PresUnit(BME280::PresUnit_hPa);
+
+    if (!bme.begin()) {
+        Serial.println("ERROR - Could not find BME sensor.");
+        // TODO: error handling
+    }
+}
+
+void SN_Sensor::displayCurrentValues() {
+    readValues();
+    int t = round(temp);
+    int h = round(hum);
+    disp.showDec(h * 100 + t);
+}
+
+void SN_Sensor::readValues() {
+    bme.read(pres, temp, hum, tempUnit, presUnit);
+}
+
+void SN_Sensor::testRead() {
+    bme.read(pres, temp, hum, tempUnit, presUnit);
+
+    Serial.println("-------------------------------");
+    Serial.print("Temperature: ");
+    Serial.println(temp);
+    Serial.print("Humidity: ");
+    Serial.println(hum);
+    Serial.print("Pressure: ");
+    Serial.println(pres);
 }
--- smart-nixie-platformio/lib/SmartNixie/SN_Sensor.h
+++ smart-nixie-platformio/lib/SmartNixie/SN_Sensor.h
@@ -3,11 +3,23 @@
 
 #include <Arduino.h>
 #include <BME280I2C.h>
+#include "SN_Display.h"
 
 class SN_Sensor {
     public:
         SN_Sensor();
+        SN_Sensor(SN_Display snDisp);
+        BME280I2C bme;
+        void displayCurrentValues();
+        void testRead();
     private:
+        BME280::TempUnit tempUnit;
+        BME280::PresUnit presUnit;
+        SN_Display disp;
+        float temp;
+        float hum;
+        float pres;
+        void readValues();
 };
 
 #endif

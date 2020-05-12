af049e1d664b4610bc9189b35cf209802b7026f3
Bene Dániel <bene.daniel@ibcontroll.hu>
2020-05-12 15:45:02 +0200

    Add basic structure for iotWebConf capture settings page parameters

--- smart-nixie-platformio/lib/SmartNixie/SN_IotWebConf.cpp
+++ smart-nixie-platformio/lib/SmartNixie/SN_IotWebConf.cpp
@@ -7,6 +7,23 @@ SN_IotWebConf::SN_IotWebConf() {
 void SN_IotWebConf::setup() {
 	Serial.println("Starting up...");
 
+	iotWebConf.setStatusPin(STATUS_PIN);
+	iotWebConf.setConfigPin(CONFIG_PIN);
+	iotWebConf.addParameter(&timeSeparator);
+	iotWebConf.addParameter(&dateTimeParam);
+	iotWebConf.addParameter(&tzidParam);
+	iotWebConf.addParameter(&turnoffSeparator);
+	iotWebConf.addParameter(&mac1Param);
+	iotWebConf.addParameter(&mac2Param);
+	iotWebConf.addParameter(&mac3Param);
+	//iotWebConf.addParameter(&stringParam);
+	//iotWebConf.addParameter(&separator1);
+	//iotWebConf.addParameter(&intParam);
+	//iotWebConf.addParameter(&separator2);
+	//iotWebConf.addParameter(&floatParam);
+	iotWebConf.setConfigSavedCallback(&configSaved);
+	iotWebConf.setFormValidator(&formValidator);
+
 	iotWebConf.init();
 
 	server.on("/", handleRoot);
@@ -26,9 +43,42 @@ void SN_IotWebConf::handleRoot() {
 	}
 
 	String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
-	s += "<title>IotWebConf 01 Minimal</title></head><body>Hello world!";
-	s += "Go to <a href='config'>configure page</a> to change settings.";
+	s += "<title>SmartNixie setup page</title></head><body>Hello world!";
+	s += "<ul>";
+	s += "<li>Time param value: ";
+	s += dateTimeParamValue;
+	s += "<li>TZID param value: ";
+	s += tzidParamValue;
+	s += "<li>MAC1 param value: ";
+	s += mac1ParamValue;
+	s += "<li>MAC2 param value: ";
+	s += mac2ParamValue;
+	s += "<li>MAC3 param value: ";
+	s += mac3ParamValue;
+	//s += "<li>Int param value: ";
+	//s += atoi(intParamValue);
+	//s += "<li>Float param value: ";
+	//s += atof(floatParamValue);
+	s += "</ul>";
+	s += "Go to <a href='config'>configure page</a> to change values.";
 	s += "</body></html>\n";
 
 	server.send(200, "text/html", s);
+}
+
+void SN_IotWebConf::configSaved() {
+	Serial.println("Configuration was updated.");
+}
+
+boolean SN_IotWebConf::formValidator() {
+	Serial.println("Validating form.");
+	boolean valid = true;
+
+	int l = server.arg(tzidParam.getId()).length();
+	if (l < 3) {
+		tzidParam.errorMessage = "Please provide at least 3 characters for this test!";
+		valid = false;
+	}
+
+	return valid;
 }
--- smart-nixie-platformio/lib/SmartNixie/SN_IotWebConf.h
+++ smart-nixie-platformio/lib/SmartNixie/SN_IotWebConf.h
@@ -4,12 +4,39 @@
 #include <Arduino.h>
 #include <IotWebConf.h>
 
+#define CONFIG_VERSION "asd0"
+#define CONFIG_PIN D5
+#define STATUS_PIN LED_BUILTIN
+
+#define STRING_LEN 128
+#define NUMBER_LEN 32
+
 const char thingName[] = "smart-nixie";
 const char wifiInitialApPassword[] = "123456";
 static DNSServer dnsServer;
 static WebServer server(80);
 
 static IotWebConf iotWebConf(thingName, &dnsServer, &server, wifiInitialApPassword);
+
+static char dateTimeParamValue[STRING_LEN];
+static char tzidParamValue[STRING_LEN];
+static char mac1ParamValue[STRING_LEN];
+static char mac2ParamValue[STRING_LEN];
+static char mac3ParamValue[STRING_LEN];
+//static char stringParamValue[STRING_LEN];
+//static char intParamValue[NUMBER_LEN];
+//static char floatParamValue[NUMBER_LEN];
+static IotWebConfSeparator timeSeparator = IotWebConfSeparator("Manual/Auto time settings");
+static IotWebConfParameter dateTimeParam = IotWebConfParameter("Current time for manual setup", "dateTimeParam", dateTimeParamValue, STRING_LEN, "text", "2020.12.25 13:30:25", NULL);
+static IotWebConfParameter tzidParam = IotWebConfParameter("Timezone for automatic setup", "tzidParam", tzidParamValue, STRING_LEN, "text", "Europe/Budapest", NULL);
+static IotWebConfSeparator turnoffSeparator = IotWebConfSeparator("Auto turnoff settings");
+static IotWebConfParameter mac1Param = IotWebConfParameter("Device 1 MAC address", "mac1Param", mac1ParamValue, STRING_LEN, "text", "12:34:56:78:9A:BC", NULL);
+static IotWebConfParameter mac2Param = IotWebConfParameter("Device 2 MAC address", "mac2Param", mac2ParamValue, STRING_LEN, "text", "12:34:56:78:9A:BC", NULL);
+static IotWebConfParameter mac3Param = IotWebConfParameter("Device 3 MAC address", "mac3Param", mac3ParamValue, STRING_LEN, "text", "12:34:56:78:9A:BC", NULL);
+//static IotWebConfSeparator separator1 = IotWebConfSeparator();
+//static IotWebConfParameter intParam = IotWebConfParameter("Int param", "intParam", intParamValue, NUMBER_LEN, "number", "1..100", NULL, "min='1' max='100' step='1'");
+//static IotWebConfSeparator separator2 = IotWebConfSeparator("Calibration factor");
+//static IotWebConfParameter floatParam = IotWebConfParameter("Float param", "floatParam", floatParamValue, NUMBER_LEN, "number", "e.g. 23.4", NULL, "step='0.1'");
 
 class SN_IotWebConf {
     public:
@@ -18,6 +45,8 @@ class SN_IotWebConf {
 		void doLoop();
 
     private:
+        static void configSaved();
+        static boolean formValidator();
         static void handleRoot();   // methods provided to server.on must be static (or binded, but you dont want that)
 
 };
